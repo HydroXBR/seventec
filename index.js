3071e36e4834936d9800733baa4981d145aac5f9
Psy <72590492+HydroXBR@users.noreply.github.com>
2025-02-27 16:17:49 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import { join, basename, dirname } from "path"
+
+import * as path from 'path'
+import { fileURLToPath } from 'url';
+const { token } = process.env
+import express from 'express'
+import cors from 'cors'
+const { PORT } = process.env || 3000
+const app = express()
+import bodyParser from "body-parser"
+const __dirname = dirname(fileURLToPath(import.meta.url))
+import Db from "mongodb"
+import im from "./db_connect.js"
+const ec = txt => encodeURIComponent(txt)
+const dec = txt => decodeURIComponent(txt)
+
+const fetch = s => import('node-fetch').then(({default: fetch}) => fetch(s))
+im()
+
+const headers = /** @type {import("http").OutgoingHttpHeaders} */ ({
+		"Access-Control-Allow-Origin": "https://brainly.com.br",
+	"Access-Control-Allow-Methods":"GET",
+	"Access-Control-Allow-Headers":"X-Api-Token"
+})
+
+app.use(
+	cors({ 
+		exposedHeaders: [
+			'Authorization'
+		]
+	}),
+	bodyParser.json(),
+	bodyParser.urlencoded({
+		extended: true
+	}),
+	express.static(path.join(__dirname, '/interface'))
+)
+
+app.listen(PORT, () => {console.log(`Listening at ${PORT}`)})
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/interface'))
+
+app.get('/',function(req,res) {
+	console.log("Access PRINCIPAL: "+ new Date())
+	res.sendFile(__dirname + '/interface/index.html')
+})
+
+app.get('/#',function(req,res) {
+	console.log("Access PRINCIPAL: "+ new Date())
+	res.sendFile(__dirname + '/interface/index.html')
+})
